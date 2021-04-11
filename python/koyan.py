f6947ae8deb85b401e16c541f910430803d3cb68
Kanyao1230 <kanaotakumi@gmail.com>
2021-04-11 18:01:04 +0900

    koyan.pyを追加

---
+++ python/koyan.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*- 
+# くきゅりん専用カスタマイズ
+
+koyans = {}
+
+koyans['?ねこ']='にゃーん\n'
+koyans['?neko']='にゃーん\n'
+
+koyans['?きつね']='こゃーん\n'
+koyans['?kitsune']='こゃーん\n'
+
+koyans['?えび']='えびです。\n'
+koyans['?ebi']='えびです。\n'
+
+koyans['?ﾃﾚｰ']='ﾃﾚｰ ﾃﾚﾚﾃｰﾚｰ♪\n'
+
+koyans['?うさぎ']='「ご注文はうさぎですか？BLOOM」\nBlu-ray & DVD 第5巻は4月28日発売！\n'
+koyans['?usagi']='「ご注文はうさぎですか？BLOOM」\nBlu-ray & DVD 第5巻は4月28日発売！\n'
+
+koyans['?うに']='うにって言われたからお風呂はいってくるわ\n'
+koyans['?uni']='うにって言われたからお風呂はいってくるわ\n'
+
+koyans['?阪急']='車内ではマナーを守り快適な車内環境づくりにご協力をお願いします。\n Thank you for making the ride a pleasant one by showing consideration to others.\n'
+
+koyans['?まんちぇ']='まーじゃんさんまたまんちぇしてるよ\n'
+koyans['?manche']='まーじゃんさんまたまんちぇしてるよ\n'
+
+koyans['?ahakuoku']='やーい　えっふぇるえっふぇるー\n'
+
+koyans['?知っておこう']='（♪ahakuokuのテーマ）\n今日は　Simutrans World Monitorについて　知っておこう\n'
+
+koyans['?うーん']='うーん\nじゃっ！\n（♪ahakuokuのテーマ）'
+
+koyans['?tanyao1230']='たんやお　1000点\n'
+
+koyans['?sss']='はいキック\n'
